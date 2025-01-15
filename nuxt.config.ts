b6b10e9620d24537d618024516d3e35dfd205289
pietro <pietro@playpreso.com>
2025-01-15 18:59:04 +0100

    compatibily date?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+
   css: [
     '~/assets/css/tailwind.css' // <-- IMPORTANT! Ensures Tailwind is injected globally
-  ]
+  ],
+
+  compatibilityDate: '2025-01-13'
 })
