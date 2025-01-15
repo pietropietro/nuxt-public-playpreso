309a77ac96b54927015151f238c866018f0308c3
pietro <pietro@playpreso.com>
2025-01-15 19:43:14 +0100

    google store badge

--- pages/index.vue
+++ pages/index.vue
@@ -49,18 +49,18 @@
         
 
         <!-- Container for the badges -->
-        <div class="flex gap-4 items-end mt-6 md:mt-0 md:items-center justify-center md:justify-start">
+        <div class="flex gap-4 items-end mt-6  md:items-center justify-center md:justify-start">
           <!-- iOS Badge Container -->
           <a 
             href="https://apps.apple.com/it/app/playpreso/id6476687916?l=en-GB" 
             target="_blank" 
             aria-label="Download on the App Store"
-            class="flex items-center justify-center h-24  w-100 bg-transparent"
+            class="flex items-center justify-center"
           >
             <img 
               src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" 
               alt="Download on the App Store" 
-              class="max-h-full max-w-full object-contain border-0"
+              class="h-10 w-auto object-contain border-0"
               loading="lazy"
             />
           </a>
@@ -70,12 +70,12 @@
             href="https://play.google.com/store/apps/details?id=com.playpreso.app" 
             target="_blank" 
             aria-label="Get it on Google Play"
-            class="flex items-center justify-center h-24  w-100 bg-transparent"
+            class="flex items-center justify-center"
           >
             <img 
-              src="https://storage.googleapis.com/pe-portal-consumer-prod-wagtail-static/images/googleplay-badge-01-getit.max-1920x1070.format-webp.webp?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=wagtail%40pe-portal-consumer-prod.iam.gserviceaccount.com%2F20250112%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20250112T150815Z&X-Goog-Expires=86400&X-Goog-SignedHeaders=host&X-Goog-Signature=70bf85fcbd69c23e0f18afdd8178c54b483bcd90d9a65fd4d8d300c34d3c323e48fc8cb732354198eb1afd78613cbdd9dd852eca65ac05a89ab955eea89bc14faa9d73de8cad2faf4e983ff3b44a0b76d59fb9f1933484336035df17ce770b05a6b1fd299966300b38c50e41dd3752601495f7b4a81f9b1b6ae9b9a8afd10a1792775d2659adfaf357f333184735e1d10a73aea95a485f5b02c30c38212b5e3af03ea115c34bea5ca09cd81e759a966dc401a3be8af2231e63d4553e435a79283bffdedbe02436890d2c9bb43697b0d7a0642ae94c4e0eaef1740ebb2949edcd4e64b4fc666431b165b2feb957c2fd347238779955548d9543dd8c83546030a6" 
+              src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" 
               alt="Get it on Google Play" 
-              class="max-h-full max-w-full object-contain border-0"
+              class="h-10 w-auto object-contain border-0"
               loading="lazy"
             />
           </a>
